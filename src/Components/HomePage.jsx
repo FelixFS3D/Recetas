36fd1130e234e473e808a93aa0994f3d97d383b7
Félix Romero <felix.fs3d@gmail.com>
2024-07-09 18:09:18 +0200

    Final dia uno

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,21 +1,26 @@
-
-import logo from '../assets/Images/Gorro.png'
+import logo from "../assets/Images/Logo1.png";
 function HomePage() {
-
   return (
     <div>
-       <nav className="navBar">
+      <nav className="navBar">
         <h1>COCINA CON SABOR</h1>
-        <img src={logo} alt="" width={"80px"}/>
-        </nav> 
+        <img src={logo} alt="" width={"70px"} />
+      </nav>
 
-        
+      <aside className="aside">
+        <br />
+        <a href="/">Pagina de inicio</a>
+        <br />
+        <a href="/">About</a>
+      </aside>
 
-        <aside className="aside"><br /><a href="" >Pagina de inicio</a><br/><a href="">About</a></aside>
-
-        <footer className="footer"><a href="https://github.com/FelixFS3D/Recetas.git">Repositorio GitHub</a></footer>
+      <footer className="footer">
+        <a href="https://github.com/FelixFS3D/Recetas.git">
+          Repositorio GitHub
+        </a>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
